c222007c22a3a8cd763a3c5d8e49fb6f6f3b45c2
ДИТ - Отдел поддержки пользователей НИУ ВШЭ <nellikhlyustova@gmail.com>
2017-11-03 11:31:47 +0300

    Migrations added

---
+++ Library.Data/Migrations/201711030829366_New.cs
@@ -0,0 +1,41 @@
+namespace Library.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class New : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Author_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Authors", t => t.Author_Id)
+                .Index(t => t.Author_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Books", "Author_Id", "dbo.Authors");
+            DropIndex("dbo.Books", new[] { "Author_Id" });
+            DropTable("dbo.Books");
+            DropTable("dbo.Authors");
+        }
+    }
+}
---
+++ Library.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Library.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Library.Data.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Library.Data.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
