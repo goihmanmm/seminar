70e388113ec6d643e8fc3cb8f8430afe95971cb0
Inkognito <goihman@mail.ru>
2017-11-03 11:20:39 +0300

    Db creation

---
+++ Library.Data/Context.cs
@@ -0,0 +1,20 @@
+﻿using Library.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class Context: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        public Context(): base("DbLibrary") { }
+
+
+    }
+}
--- Library.Data/Model/Book.cs
+++ Library.Data/Model/Book.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Library.Data
+namespace Library.Data.Model
 {
-    public class Class1
+    public class Book
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
     }
 }
